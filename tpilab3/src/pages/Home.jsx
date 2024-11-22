1aa59788d6974f0e0328b6ae6f5609c30dbdd95c
Felipe Garcia Suez <garciasuezfelipe@gmail.com>
2024-11-22 12:23:42 -0300

    Agregado register

--- tpilab3/src/pages/Home.jsx
+++ tpilab3/src/pages/Home.jsx
@@ -42,6 +42,17 @@ const Home = () => {
   return (
     <div id="main-wrapper" className="d-flex flex-column min-vh-100">
       <NavBar />
+      <div
+        className="w-100"
+        style={{
+          backgroundImage:
+            "url('https://acdn.mitiendanube.com/stores/004/690/284/themes/recife/2-slide-1724688590539-2645534423-9593b503bb01c262b0ac5dd856528e4f1724688594-1920-1920.webp?673934867')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "700px", // Asegura que el banner se vea debajo del NavBar
+        }}
+      >
+      </div>
       <div className="flex-grow-1 d-flex justify-content-center align-items-center">
         <div className="container-fluid text-center">
           <h2 className="mt-4 mb-3">TIENDA ROSARIO</h2>
